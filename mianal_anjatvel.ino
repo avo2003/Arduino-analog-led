eb0605a3bee2af8f34ceb3f4f580caddcc34529e
avo2003 <50109997+avo2003@users.noreply.github.com>
2019-07-10 01:17:08 +0400

    Add files via upload

---
+++ mianal_anjatvel.ino
@@ -0,0 +1,29 @@
+int led = 6;
+int value = 0;
+int val = 250;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led, OUTPUT);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  while(value < 250){
+  analogWrite(led, value);
+  value += 10;
+  delay(100);
+  }
+  if(value > 0){
+      value = 0;
+    }
+    while(val >= 0){
+  analogWrite(led, val);
+  val -= 10;
+  delay(100);
+  }
+    if(val <= 250){
+      val = 250;
+    }
+}
