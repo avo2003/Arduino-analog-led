21c4d87b456aac3a77c281b54428351cc59045a8
avo2003 <50109997+avo2003@users.noreply.github.com>
2019-07-10 01:59:12 +0400

    Add files via upload

---
+++ sketch_jun28a.ino
@@ -0,0 +1,15 @@
+int led=6;
+void setup() {
+    pinMode(led, OUTPUT);
+}
+
+void loop() {
+  for (int i = 0; i <= 255; i+=10) {
+    analogWrite(led, i);
+    delay(10);
+    }
+      for (int i = 255; i >= 0; i-=10) {
+    analogWrite(led, i);
+    delay(10);
+    }
+}
